ca63f19f93e0ae012cd8ef28beb3016d608a4760
Nikita <tr_nikita@mail.ru>
2023-06-07 00:01:39 +0700

    fix about block

--- src/widgets/About/ui/About.tsx
+++ src/widgets/About/ui/About.tsx
@@ -1,7 +1,9 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
+import { Image } from '@/shared/ui/Image/Image';
 import cls from './About.module.scss';
+import Photo from '../../../../public/photo.png';
 
 interface AboutProps {
 	id: string;
@@ -15,13 +17,16 @@ export const About = memo((props: AboutProps) => {
 
 	return (
 		<div id={id} className={classNames(cls.About, {}, [className])}>
-			<div>
+			<h1 className={cls.textBlock}>
 				{/*
 				чем ты можешь быть реально полезен для бизнеса
 
 				разрабатывать надежные стабильные масштабируемые проекты
 				*/}
 				Hi 👋, I'm Nikita Trokhov Frontend Developer
+			</h1>
+			<div className={cls.imageBlock}>
+				<Image src={Photo} />
 			</div>
 		</div>
 	);
