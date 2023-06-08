3c4aa5aa5ce5da47999994abcb39b6aa7e001556
Nikita <tr_nikita@mail.ru>
2023-06-08 23:29:32 +0700

    add photos

--- src/widgets/About/ui/About.tsx
+++ src/widgets/About/ui/About.tsx
@@ -2,10 +2,10 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Deck } from '@/shared/ui/Deck/Deck';
-import { Image } from '@/shared/ui/Image/Image';
 import { Text, TextAlign, TextSize } from '@/shared/ui/Text/Text';
 import cls from './About.module.scss';
-import Photo from '../../../../public/photo.png';
+import Photo_1 from '../../../../public/photo1.png';
+import Photo_2 from '../../../../public/photo2.png';
 
 interface AboutProps {
 	id: string;
@@ -16,11 +16,7 @@ export const About = memo((props: AboutProps) => {
 	const { t } = useTranslation();
 	const { className, id } = props;
 
-	const cards = [
-		'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
-		'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
-		// Photo,
-	];
+	const cards = [Photo_2, Photo_1];
 
 	return (
 		<div id={id} className={classNames(cls.About, {}, [className])}>
@@ -32,10 +28,7 @@ export const About = memo((props: AboutProps) => {
 					text={t('aboutText')}
 				/>
 			</div>
-			<div className={cls.imageBlock}>
-				<Deck />
-				<Image src={Photo} />
-			</div>
+			<Deck cards={cards} className={cls.imageBlock} />
 		</div>
 	);
 });
