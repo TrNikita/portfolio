8d3c3e28ddf978bd1aedb7c50710d2a720d4ffbf
Nikita <tr_nikita@mail.ru>
2023-06-13 23:44:30 +0700

    add photos

--- src/widgets/About/ui/About.tsx
+++ src/widgets/About/ui/About.tsx
@@ -6,6 +6,8 @@ import { Text, TextAlign, TextSize } from '@/shared/ui/Text/Text';
 import cls from './About.module.scss';
 import Photo_1 from '../../../../public/photo1.png';
 import Photo_2 from '../../../../public/photo2.png';
+import Photo_3 from '../../../../public/photo3.png';
+import Photo_4 from '../../../../public/photo4.png';
 
 interface AboutProps {
 	id: string;
@@ -16,7 +18,7 @@ export const About = memo((props: AboutProps) => {
 	const { t } = useTranslation();
 	const { className, id } = props;
 
-	const cards = [Photo_2, Photo_1];
+	const cards = [Photo_4, Photo_2, Photo_3, Photo_1];
 
 	return (
 		<div id={id} className={classNames(cls.About, {}, [className])}>
