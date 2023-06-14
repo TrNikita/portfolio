902a0779aec0fd810b40411caeab51a45e21451d
Nikita <tr_nikita@mail.ru>
2023-06-14 22:40:00 +0700

    add analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import { AppRouter } from './providers/router';
 
 const App = () => {
 	return (
 		<div className="app">
+			<Analytics />
 			<AppRouter />
 		</div>
 	);
