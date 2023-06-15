a02077dbdf3a874b13797d000de49c7a4c93c053
Nikita <tr_nikita@mail.ru>
2023-06-16 00:00:38 +0700

    fix courses card

--- src/entities/Course/ui/CourseCard.tsx
+++ src/entities/Course/ui/CourseCard.tsx
@@ -19,21 +19,19 @@ export const CourseCard = memo((props: CourseCardProps) => {
 		<div className={classNames(cls.CourseCard, {}, [className])}>
 			<Card theme={CardTheme.OUTLINED}>
 				<Card className={cls.title}>
-					<a
-						className={cls.link}
-						href={course.link}
-						target="_blank"
-						rel="noreferrer"
-					>
-						<div className={cls.title}>{course.title}</div>
-					</a>
-					<div className={cls.school}>{course.school}</div>
+					<div className={cls.title}>{course.title}</div>
+					<div className={cls.duration}>{course.duration}</div>
 				</Card>
 
-				<div className={cls.duration}>
-					<Card theme={CardTheme.SECOND}
-					>
-						{course.duration}
+				<div className={cls.school}>
+					<Card theme={CardTheme.SECOND}>
+						<a
+							href={course.link}
+							target="_blank"
+							rel="noreferrer"
+						>
+							{course.school}
+						</a>
 					</Card>
 				</div>
 
